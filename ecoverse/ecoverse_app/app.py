b71d03a65ced46d9c01aefae48ace12fbdf77d9e
K A R T H I K K R I S H N A N GS <coderkarthik01@gmail.com>
2025-07-11 16:39:30 +0530

    Update app.py

--- ecoverse/ecoverse_app/app.py
+++ ecoverse/ecoverse_app/app.py
@@ -197,4 +197,5 @@ def about_ecoverse():
 
 # Run the server
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
